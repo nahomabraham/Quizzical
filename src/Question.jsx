def669352e8ccfb6271d4669029bea5f0171ad29
Nahom Abraham <nahomabraham17@gmail.com>
2023-04-01 18:30:29 +0300

    Add choice component and array of choices to render

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import Choice from "./Choice";
 
 export default function Question(){
+
+    const choiceElements = []
+    for (let i = 0; i < 4; i++) {
+        choiceElements.push(<Choice name={`choice ${i+1}`}/>);
+        
+    }
     return (
         <div className="question">
             <div className="question-title">Question</div>
             <div className="question-choices">
-                <div className="choice">A</div>
-                <div className="choice">B</div>
-                <div className="choice">C</div>
-                <div className="choice">D</div>
+            {choiceElements}
             </div>
             <hr />
         </div>
